a6b4955408fe9f0b871bdbb24cc0dc46d9f33b02
padlesky <padlesky@ualberta.ca>
2014-03-11 19:04:09 -0600

    Added somemore functionality to the main function. Also added the quit command to the main function for when you are done making quieries to the database.

--- Project1/src/Main.java
+++ Project1/src/Main.java
@@ -11,21 +11,35 @@ public class Main{
         String m_username = grabUsername(cns);
         String m_password = grabPassword(cns);
         boolean c_password = false;
-
+        
+    	while (c_password == false) {
+	        try{
+	            cns.printf("In try.\n");
+	            Class drvClass = Class.forName(m_driverName);
+	            Connection con = DriverManager.getConnection(m_url,
+	                                m_username,m_password);
+	            con.close();
+	            c_password = true;
+	        }
+	        catch(Exception e){
+	        	cns.printf("Wrong username or password.\n"+"Plese try again.\n");
+	        	grabUsername(cns);
+	            grabPassword(cns);
+	        }
+    	}
+    	
+    	cns.printf("\n\n Enter the word 'quit' when you want to exit.\n");
+    	cns.printf("Enter one of the 5 commands to perform an action.\n");
+    	cns.printf("'nvReg' for a new vehicle registrations.\n");
+    	cns.printf("'autoT' for an automobile transaction.\n");
+    	cns.printf("'dlReg' for a driver license registration.\n");
+    	cns.printf("'vRecord' for a violation record.\n");
+    	cns.printf("'search' for a search.\n");
+    	String input;
         while (true) {
-        	while (c_password == false) {
-		        try{
-		            Class drvClass = Class.forName(m_driverName);
-		            Connection con = DriverManager.getConnection(m_url,
-		                                m_username,m_password);
-		            con.close();
-		            c_password = true;
-		        }
-		        catch(Exception e){
-		        	cns.printf("Wrong username or password.\n");
-		        	grabUsername(cns);
-		            grabPassword(cns);
-		        }
+        	input = cns.readLine();
+        	if (input == "quit") {
+        		break;
         	}
         }
     }
