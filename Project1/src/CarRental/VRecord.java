c75641be233477433ece1e538ae95a41f99226b1
yuentung <yuentung@ui04.cs.ualberta.ca>
2014-03-21 15:55:13 -0600

    part 4, done!!! i think so, test for it if you want to :)

--- Project1/src/CarRental/VRecord.java
+++ Project1/src/CarRental/VRecord.java
@@ -19,52 +19,75 @@ public class VRecord {
 		
 		cns.printf("\n\nWelcome to the Violatin Record function\n");
 		while (a=true) {
-			String ticket_no = cns.readLine("Please enter the ticket number: ");
-			if (ticket_no.equals("exit")) {
+			String office_no = cns.readLine("Please enter the officer's number: ");
+			if (office_no.equals("exit")) {
 				break;
 			}
-			if (ticket_no.equals("")) {
+			if (office_no.equals("")) {
 				System.out.println("This is Empty.");
 			} else {
 				try {
-					ResultSet check_ticket = database.create_statement("SELECT ticket_no FROM ticket WHERE ticket_no = '" + ticket_no + "'");
-						if (check_ticket.next()) {
-							System.out.println("This ticket is already entered");
-						} else {
-													
-							String violator_no = cns.readLine("Please enter the violator's number: ");
+					//check if the officer number is valid or not
+					ResultSet check_office = database.create_statement("SELECT sin FROM people WHERE sin = '" + office_no + "'");
+						while (!(check_office.next())) {
+							System.out.println("Invaid officer number.");
+							office_no = cns.readLine("Please enter the officer's number: ");
+							check_office = database.create_statement("SELECT sin FROM people WHERE sin = '" + office_no + "'");
+						}
+
+						String vehicle_id = cns.readLine("Please enter the vehicle number: ");
+						//check if the vehicle id is valid or not
+						ResultSet check_vehicle = database.create_statement("SELECT vehicle_id FROM owner WHERE vehicle_id = '" + vehicle_id + "'");
+						while (!(check_vehicle.next())) {
+							System.out.println("Invaid vehicle id.");
+							vehicle_id = cns.readLine("Please enter the vehicle number: ");
+							check_vehicle = database.create_statement("SELECT serial_no FROM vehicle WHERE serial_no = '" + vehicle_id + "'");
+						}
+						
+						String identifiable = cns.readLine("Is the violator identifiable?");
+						String violator_no = "";
+						//while(identifiable != null){
+						if(identifiable.toLowerCase().equals("y") || identifiable.toLowerCase().equals("yes")){
+							violator_no = cns.readLine("Please enter the violator's number: ");
+							
 							//check if the violator number is valid or not
 							ResultSet check_violator = database.create_statement("SELECT sin FROM people WHERE sin = '" + violator_no + "'");
 							while (!(check_violator.next())) {
-								System.out.println("Invaid violator number");
+								System.out.println("Invaid violator number.");
 								violator_no = cns.readLine("Please enter the violator's number: ");
 								check_violator = database.create_statement("SELECT sin FROM people WHERE sin = '" + violator_no + "'");
 							}
-							
-							String vehicle_id = cns.readLine("Please enter the vehicle number: ");
-							//check if the vehicle id is valid or not
-							ResultSet check_vehicle = database.create_statement("SELECT serial_no FROM vehicle WHERE serial_no = '" + vehicle_id + "'");
-							while (!(check_vehicle.next())) {
-								System.out.println("Invaid vehicle id.");
-								vehicle_id = cns.readLine("Please enter the vehicle number: ");
-								check_vehicle = database.create_statement("SELECT serial_no FROM vehicle WHERE serial_no = '" + vehicle_id + "'");
+						}
+						else if(identifiable.toLowerCase().equals("n") || identifiable.toLowerCase().equals("no")){
+							ResultSet vio_id = database.create_statement("SELECT owner_id FROM owner WHERE vehicle_id = '" + vehicle_id + "'");
+							String res = "";
+							if(vio_id.next()){
+								res = vio_id.getString(1);
+								violator_no = res;
 							}
+						}
+						else{
+							System.out.println("Invalid input.");
+							identifiable = cns.readLine("Is the violator identifiable?");
+						}
+						//}
 							
-							String office_no = cns.readLine("Please enter the officer's number: ");
-							//check if the officer number is valid or not
-							ResultSet check_office = database.create_statement("SELECT sin FROM people WHERE sin = '" + office_no + "'");
-							System.out.println("v:"+violator_no);
-							System.out.println(":"+office_no);
-							while (office_no != null) {
-								if (violator_no==office_no && !(check_office.next())){
-									System.out.println("Invaid officer number.");
-								}
-//								if(violator_no==office_no){
-//									System.out.println("Invaid officer number.");
-//								}
-								office_no = cns.readLine("Please enter the officer's number: ");
-								check_office = database.create_statement("SELECT sin FROM people WHERE sin = '" + office_no + "'");
+						vtype= getQueryResult("select vtype from ticket_type");
+						System.out.print("Here are all the violation types,\n");
+								
+						for (int i=1;i<vtype.size();i++){
+							ab=vtype.get(i);
+							for (int j=0;j<ab.size();j++){
+								abc = ab.get(j);
+								System.out.print(abc+"\n");
 							}
+						}
+								
+						String choice=cns.readLine("Please enter the appropriate type:");
+								
+						ResultSet type = database.create_statement("select vtype from ticket_type where vtype = '" + choice + "'");
+						while (!(type.next())){
+							System.out.println("The input is invalid.");
 							
 							vtype= getQueryResult("select vtype from ticket_type");
 							System.out.print("Here are all the violation tyes,\n");
@@ -76,43 +99,29 @@ public class VRecord {
 									System.out.print(abc+"\n");
 								}
 							}
-								
-							String choice=cns.readLine("Please enter the appropriate type.");
-								
-							ResultSet type = database.create_statement("select vtype from ticket_type where vtype = '" + choice + "'");
-							while (!(type.next())){
-								vtype= getQueryResult("select vtype from ticket_type");
-								System.out.print("Here are all the violation tyes,\n");
-								
-								for (int i=1;i<vtype.size();i++){
-									ab=vtype.get(i);
-									for (int j=0;j<ab.size();j++){
-										abc = ab.get(j);
-										System.out.print(abc+"\n");
-									}
-								}
-								System.out.println("The input is invalid");
-								choice=cns.readLine("Please enter the appropriate type.");
-								
-								type = database.create_statement("select v_type from ticket_type where v_type = '" + choice + "'");
-							}
 							
-							String vdate = cns.readLine("Please enter the violation date.");
-							String place = cns.readLine("Please enter where the violation took place.");
-							String descriptions = cns.readLine("Please enter the violation description.");
-								
-							database.create_statement("INSERT INTO ticket(ticket_no, violateor_no, vehicle_id, office_no, vtype, vdate, place, descriptios)"
-									+ " VALUES('" + ticket_no +"','" + violator_no + "','" + vehicle_id + "','" + office_no + "','"
-									+ choice + "', date'" + vdate + "', '" + place + "', '" + descriptions + "')");
+							choice=cns.readLine("Please enter the appropriate type:");
+							type = database.create_statement("select vtype from ticket_type where vtype = '" + choice + "'");
 						}
+							
+						String vdate = cns.readLine("Please enter the violation date:");
+						String place = cns.readLine("Please enter where the violation took place:");
+						String des = cns.readLine("Please enter the violation description:");
+						
+						int ticket_no = count_row();
+						
+						database.create_statement("INSERT INTO ticket(ticket_no, violator_no, vehicle_id, office_no, vtype, vdate, place, descriptions)"
+								+ " VALUES('" + ticket_no +"','" + violator_no + "','" + vehicle_id + "','" + office_no + "','"
+								+ choice + "', date'" + vdate + "', '" + place + "', '" + des + "')");
+						
 									
 						//Ask if the user continue entering
 						String next_op = cns.readLine("Would you like to do another operation?");
 						while(next_op != null){
-							if(next_op.toLowerCase()=="yes" || next_op.toLowerCase()=="y"){
+							if(next_op.toLowerCase().equals("yes") || next_op.toLowerCase().equals("y")){
 								a = true;
 							}
-							else if(next_op.toLowerCase()=="no" || next_op.toLowerCase()=="n"){
+							else if(next_op.toLowerCase().equals("no") || next_op.toLowerCase().equals("n")){
 								a = false;
 								break;
 							}
@@ -157,5 +166,14 @@ public class VRecord {
         }
         return feedback;
 
+	}
+	public static int count_row() throws SQLException{
+		Db database = Db.getMeMyDBPlx();
+		ResultSet statement = database.create_statement("select count(*) from ticket");
+		int number = 0;
+		if(statement.next()){
+			number = statement.getInt(1);
+		}
+		return number;
 	}
 }
